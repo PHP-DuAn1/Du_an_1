27a85766f7e12c86550839890695cb5485d8deb9
minhah08 <minhbeer2k4@gmail.com>
2023-11-05 23:44:52 +0700

    [MinhAnh] update

--- src/pages/register/register.php
+++ src/pages/register/register.php
@@ -1,3 +1,11 @@
-<?php
-echo "hello world"
-?>
+<div class="boxleft">
+    <div class="box-title">Đăng Kí Tài Khoản</div>
+    <div class="form-account">
+        <form action="index" method="post">
+            <div>
+                Email
+                <input type="email" name="email" placeholder="email">
+            </div>
+        </form>
+    </div>
+</div>
