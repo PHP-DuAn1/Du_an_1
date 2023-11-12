3291682b3a430b67c01f4714f35fc3013ea8eed5
Minhah08 <anhlmph41966@fpt.edu.vn>
2023-11-13 02:33:34 +0700

    MinhAnh Update

--- src/pages/boxleft.php
+++ src/pages/boxleft.php
@@ -5,11 +5,4 @@
                 <a href="#"><i class="fas fa-graduation-cap"></i> Điểm</a>
                 <a href="#"><i class="fas fa-concierge-bell"></i> Dịch Vụ</a>
             </div>
-        </div><div class="box-left">
-            <div class="menu-list" id="menuList">
-                <a href="#"><i class="fas fa-bell"></i> Thông Báo</a>
-                <a href="#"><i class="far fa-calendar-alt"></i> Lịch Học</a>
-                <a href="#"><i class="fas fa-graduation-cap"></i> Điểm</a>
-                <a href="#"><i class="fas fa-concierge-bell"></i> Dịch Vụ</a>
-            </div>
         </div>
