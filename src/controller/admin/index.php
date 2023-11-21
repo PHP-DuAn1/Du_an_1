fd517ddcb80727a96f4b950c1a26c9593c904019
Viet Bach <vietbach2472004@gmail.com>
2023-11-21 22:09:52 +0700

    Fixx

--- src/controller/admin/index.php
+++ src/controller/admin/index.php
@@ -1,46 +1,109 @@
-<?php
-  include "../../pages/admin/header.php";
 
-  if(isset($_GET['act']) && ($_GET['act']!="")){
-      $act = $_GET['act'];
-      switch($act){
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../pages/admin/hy.css">
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="./hy.css">
+    <script src="https://kit.fontawesome.com/4a2609ef57.js" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    <div class="container">
+        <div class="content-left">
+      <?php  include "../../pages/admin/header.php"; ?>
 
-
-
-        // Quản lí thông báo
-        case 'qltb':
-
-          include '../notification/list.php';
-          break;
-
-        // Quản lí sinh viên
-        case 'qlsv':
-              include '../student/list.php';
-              break;
-        
-        // Quản lí giáo viên
-        case 'qlgv':
-            include '../teacher/list.php';
-            break;
+        </div>
+        <div class="content">
+          <div class="header">
+            <div class="header-left">
+              <i class="fa-solid fa-house"></i>
+              <i class="fa-solid fa-chevron-right"></i>
+              <p>Tên cột tiếp theo</p>
+            </div>
+            <div class="header-right">
+              <i class="fa-solid fa-user"></i>
+              <p>
+               admin
+              </p>
+            </div>
+          </div>
+          <div class="content-content">
+          
+            <?php
+              if(isset($_GET['act']) && ($_GET['act']!="")){
+                $act = $_GET['act'];
+                switch($act){
+          
+                    case 'home':
+          
+                        include '../home/home.php';
+                        break;
+          
+                  // Quản lí thông báo
+                  case 'qlNotification':
+          
+                    include './notification/list.php';
+                    break;
+          
+                  // Quản lí sinh viên
+                  case 'qlStudent':
+                        include './student/list.php';
+                        break;
+                  
+                  // Quản lí giáo viên
+                  case 'qlTeacher':
+                      include './teacher/list.php';
+                      break;
+                      
+                  case 'qlPoint':
+                    include './point/list.php';
+                    break;    
+          
+                  case 'qlMajor':
+                    include './major/list.php';
+                    break;
+                
+                         
+                  
+                  default:
+                    include '../../pages/admin/home.php';
+                    break;   
+                }
+            } else {
+                include '../../pages/admin/home.php';
+            }
             
-        case 'qld':
-          include '../point/list.php';
-          break;    
+            ?>
+          </div>
+        </div>
+      </div>
 
-        case 'qlcn':
-          include '../major/list.php';
-          break;
-      
-        case 'qll':
-          include '../class/list.php';
-          break;        
-        
-        default:
-          include '../../pages/admin/home.php';
-          break;   
-      }
-  } else {
-      include '../../pages/admin/home.php';
-  }
+    <script>
+        function handleMenuClick(action) {
+            window.location.href = 'index.php?act=' + action;
+        }
 
-?>
+       
+    </script>
+  </body>
+</html>
+
+<script>
+        function handleMenuClick(action) {
+            window.location.href = 'index.php?act=' + action;
+        }
+
+       
+    </script>
+</body>
+</html>
--- src/pages/admin/layoutAdmin.php
+++ src/pages/admin/layoutAdmin.php
@@ -4,35 +4,47 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
+    <link rel="stylesheet" href="./hy.css">
+    <script src="https://kit.fontawesome.com/4a2609ef57.js" crossorigin="anonymous"></script>
   </head>
-  <link
-    rel="stylesheet"
-    href="../../css/layoutAdmin.css
-    "
-  />
-  <script
-    src="https://kit.fontawesome.com/4a2609ef57.js"
-    crossorigin="anonymous"
-  ></script>
   <body>
     <div class="container">
-      <div class="content-left">
-        <ul class="menu">
-          <li>
-            <i class="fa-solid fa-house"></i>
-            <p>Trang chủ</p>
-          </li>
-          <li> <a href="../../controller/admin/notification/index.php"></a>Thông báo</li>
-          <li> <a href="../../controller/admin/student/index.php"></a>Quản lý sinh viên</li>
-          <li><a href="../../controller/admin/teacher/index.php"></a>Quản lý giáo viên</li>
-          <li><a href="../../controller/admin/point/index.php"></a>Quản lý điểm</li>
-          <li><a href="../../controller/admin/major/index.php"></a>Quản lý chuyên ngành</li>
-          <li><a href="../../controller/admin/class/index.php"></a>Quản lý lớp</li>
-        </ul>
-      </div>
-
-      <div class="content">
-        <div class="header">
+        <div class="content-left">
+            <ul class="menu">
+                <li>
+                    <a href="index.php?act=home">
+                        <i class="fa-solid fa-house"></i>
+                        <p>Trang chủ</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?act=qlNotification" onclick="handleMenuClick('qlNotification')">
+                        <p>Thông báo</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?act=qlStudent" onclick="handleMenuClick('qlStudent')">
+                        <p>Quản lý sinh viên</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?act=qlTeacher" onclick="handleMenuClick('qlTeacher')">
+                        <p>Quản lý giáo viên</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?act=qlPoint" onclick="handleMenuClick('qlPoint')">
+                        <p>Quản lý điểm</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="index.php?act=qlMajor" onclick="handleMenuClick('qlMajor')">
+                        <p>Quản lý chuyên ngành</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="content">
           <div class="header">
             <div class="header-left">
               <i class="fa-solid fa-house"></i>
@@ -42,23 +54,28 @@
             <div class="header-right">
               <i class="fa-solid fa-user"></i>
               <p>
-                tên
+                admin
                 <?php
                  // tên admin lúc đăng nhập
-              ?>
+                ?>
               </p>
             </div>
           </div>
-        </div>
-        <div class="content-content">
-            bách
-                <?php
-                       // nút án vào đây
-                ?>
-        
+          <div class="content-content">
+          
+            <?php
+              // Nội dung của bạn ở đây
+            ?>
+          </div>
         </div>
       </div>
-      </div>
-    </div>
+
+    <script>
+        function handleMenuClick(action) {
+            window.location.href = 'index.php?act=' + action;
+        }
+
+       
+    </script>
   </body>
 </html>
