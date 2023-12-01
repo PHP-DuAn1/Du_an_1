52f0a0a2d007ecc5a1a4aab35bb3e2ed6a1bc788
Minhah08 <anhlmph41966@fpt.edu.vn>
2023-12-01 15:06:39 +0700

    fix

--- src/pages/schedule.php
+++ src/pages/schedule.php
@@ -1,4 +1,3 @@
-
 <div class="content">
 
     <div class="schedule">
@@ -6,42 +5,38 @@
         <h10 style="margin-left: 5px;">Lịch Học</h10>
     </div>
 
-<div class="content_schedule">
-<table class="table-container">
-    <thead>
-        <tr>
-            <th>STT</th>
-            <th>Ngày</th>
-            <th>Phòng</th>
-            <th>Giảng đường</th>
-            <th>Mã môn</th>
-            <th>Môn</th>
-            <th>Lớp</th>
-            <th>Giảng viên</th>
-            <th>Ca</th>
-            <th>Thời gian</th>
-            <th>Link Học trực tuyến</th>
-            <th>Chi tiết</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>1</td>
-            <td>12/11/2023</td>
-            <td>A101</td>
-            <td>Phòng 101</td>
-            <td>M001</td>
-            <td>Toán</td>
-            <td>Lớp 1A</td>
-            <td>Nguyễn Văn A</td>
-            <td>Sáng</td>
-            <td>8:00 - 10:00</td>
-            <td><a href="#">Link 1</a></td>
-            <td><a href="#">Chi tiết 1</a></td>
-        </tr>
+    <div class="content_schedule">
+        <table class="table-container">
+            <thead>
+                <tr>
+                    <th>STT</th>
+                    <th>Ngày</th>
+                    <th>Phòng</th>
+                    <th>Mã môn</th>
+                    <th>Môn</th>
+                    <th>Lớp</th>
+                    <th>Giảng viên</th>
+                    <th>Thời gian</th>
+                    <th>Link Học trực tuyến</th>
+                    <th>Chi tiết</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>12/11/2023</td>
+                    <td>A101</td>
+                    <td>M001</td>
+                    <td>Toán</td>
+                    <td>Lớp 1A</td>
+                    <td>Nguyễn Văn A</td>
+                    <td>8:00 - 10:00</td>
+                    <td><a href="#">Link 1</a></td>
+                    <td><a href="#">Chi tiết 1</a></td>
+                </tr>
 
-    </tbody>
-</table>
-</div>
+            </tbody>
+        </table>
+    </div>
 </div>
 </main>
